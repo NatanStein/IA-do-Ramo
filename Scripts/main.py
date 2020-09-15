6f6a5fd1023d1f342d614ebf6f00967c5f12e38e
Natan Steinbruch <natsteinbruch@gmail.com>
2020-09-14 21:22:28 -0300

    Correção de erro

--- Scripts/main.py
+++ Scripts/main.py
@@ -7,19 +7,20 @@ Created on Fri Sep  4 17:51:49 2020
 
 import process_dataset as pdt
 import funcao_ativacao as fa
+import numpy as np
 import cv2
 #
 #Bloco Principal
 #
-peso1 = 2 * random.random((784, 53)) -1
-peso2 = 2 * random.random((53, 36)) -1
-peso3 = 2 * random.random((36, 25)) -1
-peso4 = 2 * random.random((25, 1)) -1
+peso1 = 2 * np.random.random((784, 53)) -1
+peso2 = 2 * np.random.random((53, 36)) -1
+peso3 = 2 * np.random.random((36, 25)) -1
+peso4 = 2 * np.random.random((25, 1)) -1
 
-bias1 = np.zeros(1, 53)
-bias2 = np.zeros(1, 36)
-bias3 = np.zeros(1, 25)
-bias4 = np.zeros(1, 1)
+bias1 = np.zeros((1,53))
+bias2 = np.zeros((1,36))
+bias3 = np.zeros((1,25))
+bias4 = np.zeros((1,1))
 
 
 path = "C:\\Users\\natst\\OneDrive\\Natan Steinbruch\\IA-do-Ramo\\DataSet\\"
@@ -28,4 +29,4 @@ path = "C:\\Users\\natst\\OneDrive\\Natan Steinbruch\\IA-do-Ramo\\DataSet\\"
 train,test,dataSet,train_saidas,test_saidas = pdt.process_data_set(path)
 img = cv2.imread(path+"train/without_mask/83.jpg",0)
 img = cv2.resize(img,(28,28)) / 255
-img = img.flatten()
+img = img.flatten().reshape((1,784)) #Transforma a matriz 28x28 para um vetor linear
